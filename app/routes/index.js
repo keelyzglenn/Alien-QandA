13862d3ef9b1cb0ecb96f71abc2b9124fabc5f35
Keely Glenn <glenj392@newschool.edu>
2017-03-24 09:10:44 -0700

    fix bug with new question

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,10 +6,10 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    saveQuestion(params) {
-      var newQuestion = this.store.createRecord('question', params);
-      newQuestion.save();
-      this.transitionTo('index');
-    },
-  }
+   saveQuestion(params) {
+     var newQuestion = this.store.createRecord('question', params);
+     newQuestion.save();
+     this.transitionTo('index');
+   },
+ }
 });
