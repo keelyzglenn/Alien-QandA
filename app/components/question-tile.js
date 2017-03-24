327a592b3be34126436d57a6be508a28d16f8ec9
Keely Glenn <glenj392@newschool.edu>
2017-03-24 08:53:10 -0700

    refractor update and delete actions to question page

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
- updateQuestionForm: false,
 
- actions: {
-   update(question, params) {
-     this.sendAction('update', question, params);
-   },
- }
 });
